e9877a35ef9d6d3e7c74bdc4bb598621e679750e
Tiffany Ellis <37965785+tae7612@users.noreply.github.com>
2021-01-24 20:12:33 -0500

    Created function to load seminar data to page

---
+++ script.js
@@ -0,0 +1,24 @@
+var xhr = new XMLHttpRequest();
+
+xhr.onreadystatechange = function() {
+  if (xhr.readyState == 4 && xhr.status == 200) {
+    var obj = JSON.parse(xhr.responseText);
+    loadSeminars(obj.seminars);
+  }
+};
+xhr.open("GET", "seminar.json", true);
+xhr.send();
+
+
+
+function loadSeminars(seminarItems){
+   
+    let output ="";
+    
+    seminarItems.forEach( function (item){	
+		output +=  '<div class="col-md-6 px-3  seminar"><div class="card border-0 p-0 "><img src="assets/img/seminars/'+item.img+'" class="card-img-top" alt="..."><div class="card-body px-0"><h4 class="card-title mb-0"><a href="'+item.link+'">'+item.topic+'</a></h4><p class="professor mt-0">'+item.professor+'</p><p class="card-text">'+item.summary+'<span><a href="'+item.link+'">Click here to Learn More.</a></span> </p><div class="details"><ul class="fa-ul"><li class="my-3 align-text-top"><i class="fa-li icon calendar-icon "></i><span class="mx-2 font-weight-bold">'+item.date+'</span></li><li class="my-3"><i class="fa-li icon clock-icon"></i><span class="mx-2 font-weight-bold">'+item.time+'</span></li><li class="my-3"><i class="fa-li icon location-icon"></i><span class="mx-2 font-weight-bold">'+item.venue+'</span></li></ul></div></div></div><hr></div>';
+ 					
+ 	});
+ 	// Outputs the data to the main section
+ 	document.getElementById("seminar-container").innerHTML = output;
+}
